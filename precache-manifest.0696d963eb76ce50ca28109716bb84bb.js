fa72aa68369f4ac033b1ecca9ebc047727e06bb7
Asad Ali Baloch <asadalibaloch059@gmail.com>
2021-06-28 15:21:00 +0500

    Updates

---
+++ precache-manifest.0696d963eb76ce50ca28109716bb84bb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be76b1959ea91137d321588ef82d5949",
+    "url": "/bunq-chat-messenger/index.html"
+  },
+  {
+    "revision": "4a552ba06d8796e9f6e7",
+    "url": "/bunq-chat-messenger/static/css/2.ecfb3f68.chunk.css"
+  },
+  {
+    "revision": "ac00cbf25f3f6f50276b",
+    "url": "/bunq-chat-messenger/static/css/main.2c94a573.chunk.css"
+  },
+  {
+    "revision": "4a552ba06d8796e9f6e7",
+    "url": "/bunq-chat-messenger/static/js/2.b3b3b212.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/bunq-chat-messenger/static/js/2.b3b3b212.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac00cbf25f3f6f50276b",
+    "url": "/bunq-chat-messenger/static/js/main.c383a461.chunk.js"
+  },
+  {
+    "revision": "15caeeab6363205f1912",
+    "url": "/bunq-chat-messenger/static/js/runtime-main.a1409da2.js"
+  },
+  {
+    "revision": "8bd20c99161a395ec5c1be12c5c64a95",
+    "url": "/bunq-chat-messenger/static/media/BunqLogo.8bd20c99.svg"
+  },
+  {
+    "revision": "4deeeeb08d3d9fc7d488ac52a46633b7",
+    "url": "/bunq-chat-messenger/static/media/chatbg.4deeeeb0.png"
+  },
+  {
+    "revision": "211c1153d75a5941a214cc29314c222f",
+    "url": "/bunq-chat-messenger/static/media/done_all.211c1153.svg"
+  },
+  {
+    "revision": "963a7dccd38b4cbea3a6f06f9a6326c8",
+    "url": "/bunq-chat-messenger/static/media/group_chat.963a7dcc.svg"
+  },
+  {
+    "revision": "1c737aa4a320a0a2ae20463d0b9f6a54",
+    "url": "/bunq-chat-messenger/static/media/information.1c737aa4.svg"
+  },
+  {
+    "revision": "2655160e3b7e88c8c60c869f613acd89",
+    "url": "/bunq-chat-messenger/static/media/log-out.2655160e.svg"
+  },
+  {
+    "revision": "958bf042217447a5282c358d5c23ff03",
+    "url": "/bunq-chat-messenger/static/media/mic.958bf042.svg"
+  },
+  {
+    "revision": "7c72c73f23350d05b6e02def7d7ccafc",
+    "url": "/bunq-chat-messenger/static/media/personal_chat.7c72c73f.svg"
+  },
+  {
+    "revision": "231fed550308fbb034a7b92d0a12bb52",
+    "url": "/bunq-chat-messenger/static/media/search.231fed55.svg"
+  },
+  {
+    "revision": "282ecb9e6de3a9602f6cb600cc32239d",
+    "url": "/bunq-chat-messenger/static/media/send.282ecb9e.svg"
+  },
+  {
+    "revision": "c50cc4490e6bcb8a32f3b77676df03be",
+    "url": "/bunq-chat-messenger/static/media/tag_faces.c50cc449.svg"
+  }
+]);
